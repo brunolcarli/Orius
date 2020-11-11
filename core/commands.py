54ee5d69ab241b78f887f675602b4c201891f510
Bruno L. Carli <brunolcarli@gmail.com>
2020-11-11 20:11:43 -0300

    ADD simple message leveling up

--- core/commands.py
+++ core/commands.py
@@ -3,6 +3,8 @@ import discord
 from discord.ext import commands, tasks
 from orius.settings import __version__
 
+from core.db_tools import update_member
+
 client = commands.Bot(command_prefix='o:')
 log = logging.getLogger()
 
@@ -21,3 +23,21 @@ async def version(ctx):
     Shows Orius version
     """
     await ctx.send(__version__)
+
+
+@client.event
+async def on_message(message):
+    """
+    Chat message handler.
+    """
+    channel = message.channel
+
+    # Do not process other bot messages
+    if message.author.bot:
+        return
+
+    # Give priority for bot prefixed commands
+    await client.process_commands(message)
+
+    x = update_member(str(message.guild.id), str(message.author.id))
+    print(x)
