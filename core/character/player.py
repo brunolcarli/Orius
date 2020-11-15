4b793e08c2793278c8e45a0e4908f17fc9f8fe40
Bruno L. Carli <brunolcarli@gmail.com>
2020-11-15 16:56:18 -0300

    FIX docstrings

--- core/character/player.py
+++ core/character/player.py
@@ -2,6 +2,15 @@ from core.character.skill import Skill
 
 
 class Player:
+    """
+    A player class is a represenation of a character, member or user.
+    This class is cast over a mongo document, json or dict similiar structure
+    to construct an object with a player behavior, so it can view status,
+    skills and battle actions.
+
+    The param : **attributes** is a duck typing implementation that, as
+    mentioned before, expects a dict like object to init its self attributes.
+    """
     def __init__(self, **attributes):
         self.lv = attributes.get('lv')
         self.name = attributes.get('name')
@@ -18,24 +27,50 @@ class Player:
         self.skill_points = attributes.get('skill_points')
         self.learned_skills = attributes.get('learned_skills')
         self.skillset = attributes.get('skillset')
+        self.items = attributes.get('items')
+        self.resets = attributes.get('resets')
+        self.kills = attributes.get('kills')
+        self.deaths = attributes.get('deaths')
+        self.delay = attributes.get('delay')
 
     def __repr__(self):
+        """
+        Returns a string representation fo the class.
+        """
         return f'{self.name} Lv: {self.lv}'
 
     def is_alive(self):
+        """
+        Returns True if player is alive and False if its dead.
+        """
         return True if self.hp > 0 else False
 
     def list_skills(self):
+        """
+        Returns a <list> of <Skill> this player has learned.
+        """
         return [Skill(**skill) for skill in self.learned_skills]
 
     def get_skillset(self):
+        """
+        Returns a <list> of <Skill> this player setted for combat.
+        """
         return [Skill(**skill) for skill in self.skillset]
 
     def get_skill(self, skill_name):
+        """
+        Returns a <Skill> based on its name.
+        param: skill_name : <str>
+        """
         skill = [skill for skill in self.skills if skill.get('name') == skill_name]
         return next(Skill(**skill))
 
     def get_damage(self, damage):
+        """
+        Decreases player health points (hp) equal to damage taken.
+        Returns True if player still alive after the damage and False if dies.
+        param : damage : <int>
+        """
         self.hp -= damage
 
         if self.hp <= 0:
@@ -47,6 +82,12 @@ class Player:
         pass
 
     def attack(self, skill, target):
+        """
+        Atacks or use a skill on a target.
+        param : skill : <Skill>
+        param : target : <Player>
+        return: <dict>
+        """
         if skill.cost > self.mp:
             return {
                 'hit': False,
